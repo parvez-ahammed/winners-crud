0002968711cef98eafd740461782f6a09e08deeb
piru72 <parvezdirom2000@gmail.com>
2025-01-23 02:33:07 +0600

    add: box layout

--- client/src/features/winners/components/AddWinner.tsx
+++ client/src/features/winners/components/AddWinner.tsx
@@ -1,5 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { Button, Input, Stack } from '@chakra-ui/react';
+import { Box, Button, Input, Stack } from '@chakra-ui/react';
 import { Field } from '@/components/ui/field';
 import { Fieldset } from '@chakra-ui/react';
 import { NativeSelectField, NativeSelectRoot } from '@/components/ui/native-select';
@@ -47,72 +47,73 @@ export const AddWinner: React.FC = () => {
       teamMember2: '',
     });
 
-    // also reset the Select dropdown
     setSelectedGame('');
 
     alert('Winner added successfully');
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-      <Fieldset.Root size="lg" maxW="full" w="full">
-        <Stack>
-          <Fieldset.Legend>Winners Details</Fieldset.Legend>
-          <Fieldset.HelperText>Please provide details about the winners</Fieldset.HelperText>
-        </Stack>
+    <Box w="full" border={1} p={4} borderRadius="lg" borderWidth={2} boxShadow="lg">
+      <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+        <Fieldset.Root size="lg" maxW="full" w="full">
+          <Stack>
+            <Fieldset.Legend>Winners Details</Fieldset.Legend>
+            <Fieldset.HelperText>Please provide details about the winners</Fieldset.HelperText>
+          </Stack>
 
-        <Fieldset.Content>
-          <Field label="Season" required>
-            <NativeSelectRoot>
-              <NativeSelectField
-                placeholder="Select Season"
-                name="season"
-                items={['2024', '2025']}
-                onChange={(e) => setFormData((prev) => ({ ...prev, season: e.target.value }))}
-              />
-            </NativeSelectRoot>
-          </Field>
-          <Field label="Game name" required>
-            <NativeSelectRoot>
-              <NativeSelectField
-                name="game"
-                placeholder="Select Game"
-                items={['Chess', 'UNO', 'FoosBall', 'Table Tennis']}
-                onChange={(e) => handleGameChange(e.target.value)}
-              />
-            </NativeSelectRoot>
-          </Field>
-          <Field label="Position" required>
-            <NativeSelectRoot>
-              <NativeSelectField
-                placeholder="Select Position"
-                name="position"
-                items={['Champion', '1st Runners Up', '2nd Runners Up']}
-                onChange={(e: ChangeEvent<HTMLSelectElement>) => setFormData((prev) => ({ ...prev, position: e.target.value }))}
-              />
-            </NativeSelectRoot>
-          </Field>
-
-          {['Chess', 'UNO'].includes(selectedGame) ? (
-            <Field label="Name" required>
-              <Input name="name" value={formData.teamMember1} onChange={handleInputChange} />
+          <Fieldset.Content>
+            <Field label="Season" required>
+              <NativeSelectRoot>
+                <NativeSelectField
+                  placeholder="Select Season"
+                  name="season"
+                  items={['2024', '2025']}
+                  onChange={(e) => setFormData((prev) => ({ ...prev, season: e.target.value }))}
+                />
+              </NativeSelectRoot>
             </Field>
-          ) : (
-            <Stack direction="row" gap={4}>
-              <Field label="Team Member 1" required>
-                <Input name="teamMember1" value={formData.teamMember1} onChange={handleInputChange} />
-              </Field>
-              <Field label="Team Member 2" required>
-                <Input name="teamMember2" value={formData.teamMember2} onChange={handleInputChange} />
-              </Field>
-            </Stack>
-          )}
-        </Fieldset.Content>
+            <Field label="Game name" required>
+              <NativeSelectRoot>
+                <NativeSelectField
+                  name="game"
+                  placeholder="Select Game"
+                  items={['Chess', 'UNO', 'FoosBall', 'Table Tennis']}
+                  onChange={(e) => handleGameChange(e.target.value)}
+                />
+              </NativeSelectRoot>
+            </Field>
+            <Field label="Position" required>
+              <NativeSelectRoot>
+                <NativeSelectField
+                  placeholder="Select Position"
+                  name="position"
+                  items={['Champion', '1st Runners Up', '2nd Runners Up']}
+                  onChange={(e: ChangeEvent<HTMLSelectElement>) => setFormData((prev) => ({ ...prev, position: e.target.value }))}
+                />
+              </NativeSelectRoot>
+            </Field>
 
-        <Button type="submit" alignSelf="flex-end">
-          Submit
-        </Button>
-      </Fieldset.Root>
-    </form>
+            {['Chess', 'UNO'].includes(selectedGame) ? (
+              <Field label="Name" required>
+                <Input name="name" value={formData.teamMember1} onChange={handleInputChange} />
+              </Field>
+            ) : (
+              <Stack direction="row" gap={4}>
+                <Field label="Team Member 1" required>
+                  <Input name="teamMember1" value={formData.teamMember1} onChange={handleInputChange} />
+                </Field>
+                <Field label="Team Member 2" required>
+                  <Input name="teamMember2" value={formData.teamMember2} onChange={handleInputChange} />
+                </Field>
+              </Stack>
+            )}
+          </Fieldset.Content>
+
+          <Button type="submit" alignSelf="flex-end">
+            Submit
+          </Button>
+        </Fieldset.Root>
+      </form>
+    </Box>
   );
 };
