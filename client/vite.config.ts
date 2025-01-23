8858404e0627c5abe31cff32efbfd4549bfdbab5
Parvez Ahammed <parvezdirom2000@gmail.com>
2025-01-23 18:35:32 +0600

    add: docker config for frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tsconfigPaths()],
-})
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+});
