823fef9d8b46e9466355011377e3df24c33ebfc9
Parvez Ahammed <parvezdirom2000@gmail.com>
2025-01-28 14:19:54 +0600

    update: workflow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 8080,
     watch: {
       usePolling: true,
       interval: 100,
