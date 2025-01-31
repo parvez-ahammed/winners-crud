c6262aca863fddd2cfacd877b707d74ec8450c00
Parvez Ahammed <parvezdirom2000@gmail.com>
2025-01-31 12:45:45 +0600

    add: env to server

--- server/config/database.go
+++ server/config/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/piru72/winners-crud/server/database/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,8 +14,31 @@ var DB *gorm.DB
 
 func Connect() {
 
-	dsn := "host=localhost user=user1 password=password1 dbname=winner_crud port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	// Load environment variables from .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Println("Warning: No .env file found. Using system environment variables.")
+	}
 
+	// Read database config from environment variables
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("DB_SSLMODE")
+	timeZone := os.Getenv("DB_TIMEZONE")
+
+	// Construct DSN
+	dsn := "host=" + host +
+		" user=" + user +
+		" password=" + password +
+		" dbname=" + dbName +
+		" port=" + port +
+		" sslmode=" + sslMode +
+		" TimeZone=" + timeZone
+
+	// Connect to database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
